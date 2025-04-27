7099e38996b684a93eed44aa2fb3cc1df1f82328
Usman <robinhoodfrost360@gmail.com>
2025-04-27 14:41:32 +0500

    Add image cropping functionality for specific filenames

---
+++ Crop Multiple Images Using Python.py
@@ -0,0 +1,23 @@
+import os
+from PIL import Image
+
+def crop_images_in_folder(folder_path):
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".jpg") and "a" in filename:
+            file_path = os.path.join(folder_path, filename)
+            image = Image.open(file_path)
+            width, height = image.size
+
+            margin = int(width * 0.07)  
+
+            left_crop = image.crop((margin, 0, width // 2, height))
+            left_filename = filename.replace("a", "b")
+            left_crop.save(os.path.join(folder_path, left_filename))
+
+            right_crop = image.crop((width // 2, 0, width - margin, height))
+            right_filename = filename.replace("a", "c")
+            right_crop.save(os.path.join(folder_path, right_filename))
+
+# Seth path to the folder containing images
+folder_path = r"c:\Users\Usman\Desktop\test"
+crop_images_in_folder(folder_path)
